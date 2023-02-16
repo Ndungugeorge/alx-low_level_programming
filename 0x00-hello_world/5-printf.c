fe82c009ccfb563880bc2f3657a04c4fe966916e
George <ndungugeorge910@gmail.com>
2023-02-16 13:50:15 -0800

    c program to print a sentense using the function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * * main - print line using prinf function
+ *
+ * * Return: 0 (success) else fail
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
