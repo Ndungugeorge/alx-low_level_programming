ad0bbfa10fbe87d4c8ab51ae1d553151a892700b
George <ndungugeorge910@gmail.com>
2023-02-17 12:34:35 -0800

    print all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print all possible combinations of single-digit numbers
+ *Return: Always 0 (Success)else fail
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(ch);
+if (i != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar(10); /* new line*/
+
+return (0);
+}
