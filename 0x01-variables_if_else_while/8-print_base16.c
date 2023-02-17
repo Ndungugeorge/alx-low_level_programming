49708f4284465b37432e1d55d5f4a0008a2cec01
George <ndungugeorge910@gmail.com>
2023-02-17 12:25:05 -0800

    #include <stdio.h>

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success) else fail
+ */
+int main(void)
+{
+char i;
+int x;
+
+for (x = 48; x <= 57; x++)
+{
+
+putchar(x);
+}
+for (i = 'a'; i <= 'f'; i++)
+{
+
+putchar(i);
+}
+
+putchar(10);
+
+return (0);
+}
