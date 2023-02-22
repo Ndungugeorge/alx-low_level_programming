8d6e3641e8c195e2f60cd4ef62dbcdc7e2b0cb2e
George <ndungugeorge910@gmail.com>
2023-02-22 02:26:41 -0800

    sum of all multiples of 3or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints natural numbers below 1024 that are
+ * multiples of 3 or 5
+ *
+ * Return: return 0.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 1; i< 1024; i++)
+{
+	if ((i % 3) == 0 || (i % 5) == 0)
+	j += i;
+}
+	printf("%d\n", j);
+	return (0);
+}
