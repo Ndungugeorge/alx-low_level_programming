4badcd9aceacc385aba4f1c7758ab034b2964e4c
George <ndungugeorge910@gmail.com>
2023-04-04 21:30:50 +0300

    Delete at index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - func deletes the node at index
+ * of list.
+ * @head: head pointer.
+ * @index: index of the node to be deleted.
+ *
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *prevnode;
+	listint_t *nextnode;
+
+	prevnode = *head;
+
+	if (index != 0)
+	{
+		for (i = 0; i < index - 1 && prevnode != NULL; i++)
+		{
+			prevnode = prevnode->nextnode;
+		}
+	}
+
+	if (prevnode == NULL || (prevnode->next == NULL && index != 0))
+	{
+		return (-1);
+	}
+
+	nextnode = prevnode->nextnode;
+
+	if (index != 0)
+	{
+		prevnode->nextnode = nextnode->nextnode;
+		free(nextnode);
+	}
+	else
+	{
+		free(prevnode);
+		*head = nextnode;
+	}
+
+	return (1);
+}
