a2b4cd547c65240f40c157fe6f8410aa3b646439
George <ndungugeorge910@gmail.com>
2023-06-23 16:51:57 -0400

    variadic function update which failed betty

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,9 +1,9 @@
 #include "variadic_functions.h"
-#include<stdio.h>
-#include<stdarg.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_strings - prints stringsfollowed by new line.
+ * print_strings - prints strings followed by a new line.
  * @separator: string to be printed between the strings.
  * @n: number of strings passed to the function.
  *
@@ -17,24 +17,24 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	if (n > 0)
 	{
+		va_start(lst, n);
 
-	va_start(lst, n);
+		for (i = 0; i < n; i++)
+		{
+			str = va_arg(lst, char *);
 
-	for (i = 0; i < n; i++)
-	{
-		str = va_arg(lst, char *);
+			if (str != NULL)
+				printf("%s", str);
+			else
+				printf("(nil)");
 
-		if (str != NULL)
-			printf("%s", str);
-		else
-			printf("(nil)");
-
-		if (separator !=NULL && i != (n - 1))
+			if (separator != NULL && i != (n - 1))
 				printf("%s", separator);
+		}
+
+		va_end(lst);
 	}
 
-	va_end(lst);
-	
-	}
 	printf("\n");
 }
+
